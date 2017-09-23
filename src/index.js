66ef4a1bf38ba3cbcb92a9dd6cb7cdf5172121a9
Beaglefoot <ch-e-st@yandex.ru>
2017-09-23 14:11:17 +0300

    Map meteorites landing locations

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import {
   svg as svgClass,
   water,
   globe as globeClass,
-  land as landClass
+  land as landClass,
+  meteorite
 } from './index.scss';
 
 const d3 = require('d3');
@@ -35,48 +36,61 @@ const projection = d3.geoOrthographic()
   .scale(scale)
   .clipAngle(90);
 
-const path = d3.geoPath()
-  .projection(projection);
+const path = d3.geoPath(projection);
+const circle = d3.geoCircle().radius(1);
 
 
 
-import('./world-110m').then(world => {
-  const land = topojson.feature(world, world.objects.land);
+fetch(url)
+  .then(response => response.json())
+  .then(({ features: meteorites }) => (
+    meteorites = meteorites.filter(({ geometry }) => geometry).filter((_, i) => !(i % 6))
+  ))
+  .then(meteorites => new Promise(resolve => (
+    import('./world-110m').then(world => resolve({ meteorites, world }))
+  )))
+  .then(({ meteorites, world }) => {
+    const land = topojson.feature(world, world.objects.countries);
 
-  const drawWorld = () => {
-    const globe = svg.append('g').classed(globeClass, true);
+    const drawWorld = () => {
+      const globe = svg.append('g').classed(globeClass, true);
 
-    globe.append('circle')
-      .classed(water, true)
-      .attr('cx', width / 2)
-      .attr('cy', height / 2)
-      .attr('r', radius);
+      globe.append('circle')
+        .classed(water, true)
+        .attr('cx', width / 2)
+        .attr('cy', height / 2)
+        .attr('r', radius);
 
-    globe.datum(land)
-      .append('path')
-      .attr('d', path)
-      .classed(landClass, true);
-  };
+      globe.datum(land)
+        .append('path')
+        .attr('d', path)
+        .classed(landClass, true);
 
-  drawWorld();
-
-  const rotate = (() => {
-    let accumulatedRotation = { x: 0, y: 0 };
-
-    return (x, y) => {
-      accumulatedRotation.x += x * rotationModifier;
-      accumulatedRotation.y -= y * rotationModifier;
-
-      svg.select(`.${globeClass}`).remove();
-      projection.rotate([accumulatedRotation.x, accumulatedRotation.y]);
-      drawWorld();
+      globe
+        .selectAll().data(meteorites)
+        .enter().append('path')
+        .attr('d', d => path(circle.center(d.geometry.coordinates)()));
     };
-  })();
 
-  svg.call(
-    d3.drag()
-      .on('start', () => console.log('drag start'))
-      .on('drag', () => rotate(d3.event.dx, d3.event.dy))
-      .on('end', () => console.log('drag end'))
-  );
-});
+    drawWorld();
+
+    const rotate = (() => {
+      let accumulatedRotation = { x: 0, y: 0 };
+
+      return (x, y) => {
+        accumulatedRotation.x += x * rotationModifier;
+        accumulatedRotation.y -= y * rotationModifier;
+
+        svg.select(`.${globeClass}`).remove();
+        projection.rotate([accumulatedRotation.x, accumulatedRotation.y]);
+        drawWorld();
+      };
+    })();
+
+    svg.call(
+      d3.drag()
+        .on('start', () => console.log('drag start'))
+        .on('drag', () => rotate(d3.event.dx, d3.event.dy))
+        .on('end', () => console.log('drag end'))
+    );
+  });
